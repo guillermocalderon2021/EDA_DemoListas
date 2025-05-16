4d1d21d2274a7d4b7d0fb4016719f1fcee4e03ad
guillermocalderon2021 <guillermo.calderon@udb.edu.sv>
2025-05-16 11:01:32 -0600

    Agregando eliminaciones

--- DemoListas/LinkedList.cs
+++ DemoListas/LinkedList.cs
@@ -72,14 +72,96 @@ namespace DemoListas
                 first = last = aux;
                 return;
             }
+            if (pos <= 0)
+            {
+                AddToStart(value);
+                return;
+            }
+
             //Si la lista no esta vacia
             Node n1 = FindAt(pos - 1);
+            if (n1 == null)
+            {
+                Add(value);// Insertando al final de la lista
+                return;
+            }
             Node n2 = n1.Next;
             aux.Next = n2;
             n1.Next = aux;
 
         }
+        // devuelve el indice de la primera ocurrencia de value en la lista
+        public int Find(int value)
+        {
+            if (IsEmpty())
+            {
+                return -1;
+            }
+            Node current = first;
+            int index = 0;
+            while (true)
+            {
+                if (current.Data == value) return index;
+                current = current.Next;
+                index++;
+                if (current == null) break;//Cuando ya no hayan mas nodos
+            }
+            return -1;
+        }
 
+        public int Length()
+        {
+            if (IsEmpty()) return 0;
+            int counter = 0;
+            Node current = first;
+            while (true)
+            {
+                counter++;
+                current = current.Next;
+                if (current == null) break;
+            }
+            return counter;
+        }
+
+        public void DeleteFirst()
+        {
+            if (IsEmpty()) return;
+            Node aux = first;
+            first=first.Next;
+            aux.Next = null;
+            
+        }
+
+        public void DeleteLast()
+        {
+            if (IsEmpty()) return;
+            if (Length() == 1)
+            {
+                DeleteFirst();
+                return;
+            }
+            Node aux = last;
+            last = FindAt(Length() - 2);
+            last.Next = null;
+        }
+
+        public void DeleteAt(int pos)
+        {
+            if (IsEmpty()) return;
+            if (pos < 0 || pos >= Length()) return;
+            if (pos == 0)
+            {
+                DeleteFirst();
+                return;
+            }
+
+            Node aux = FindAt(pos);
+            Node nxt = aux.Next;
+            Node prev = FindAt(pos - 1);
+            prev.Next = nxt;
+            aux.Next = null;
+
+        }
 
 
         public void Show()
--- DemoListas/Program.cs
+++ DemoListas/Program.cs
@@ -12,15 +12,17 @@ namespace DemoListas
         {
             LinkedList myList = new LinkedList();
             myList.Add(10);
-            myList.AddToStart(5);
             myList.Add(20);
             myList.Add(30);
             myList.AddToStart(1);
-            
             myList.InsertAt(2, 0);
             myList.InsertAt(100, 100);
+            myList.DeleteFirst();
             myList.Show();
             Console.WriteLine("---------------");
+            Console.WriteLine(myList.Find(20));
+            Console.WriteLine(myList.Find(100));
+            Console.WriteLine(myList.Find(55));
 
             Console.ReadKey();
         }
